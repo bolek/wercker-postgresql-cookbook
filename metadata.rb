fa3956cd820f5a0b084256b0177f040af92b49ab
wwwouter <wouter@wercker.com>
2013-01-30 15:28:44 +0100

    metadata

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'wercker-postgresql-cookbook'
+maintainer       'wercker'
+maintainer_email 'pleasemailuse@wercker.com'
+license          'MIT'
+description      'Setup postgresql'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
